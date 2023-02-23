dcadb6d59e13339ed05e107f95e3036a705586a3
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 14:35:31 +0530

    Added: Code to get number between 1 to 6

--- src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
+++ src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
@@ -2,6 +2,9 @@ package com.bridgelabz.snake_ladder;
 
 public class SnakeLadderGame {
     public static void main(String[] args) {
+
         int player1position = 0;
+        int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
+        System.out.print(number);
     }
 }
