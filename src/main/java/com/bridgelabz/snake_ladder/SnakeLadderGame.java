464eecb8641c40f543785e076ad9cc9eba1e6267
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 14:59:00 +0530

    Added: Code to get options to play the game

--- src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
+++ src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
@@ -5,6 +5,18 @@ public class SnakeLadderGame {
 
         int player1position = 0;
         int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
-        System.out.print(number);
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        switch (option) {
+            case 0:
+                player1position = player1position;
+                break;
+            case 1:
+                player1position += number;
+                break;
+            case 2:
+                player1position -= number;
+                break;
+        }
+        System.out.println(player1position);
     }
 }
