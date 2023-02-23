042d5475d5ed644613d8ea644ea6dff910dd7521
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 16:08:47 +0530

    Added: Code to get no of times dice rolled and position after each rolling

--- src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
+++ src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
@@ -2,9 +2,10 @@ package com.bridgelabz.snake_ladder;
 
 public class SnakeLadderGame {
     public static void main(String[] args) {
-        int player1position = 0;
+        int player1position = 0, diceRollCounter = 0;
         while (player1position < 100) {
             int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
+            diceRollCounter++;
             int option = (int) Math.floor(Math.random() * 10) % 3;
             switch (option) {
                 case 0:
@@ -25,6 +26,7 @@ public class SnakeLadderGame {
             if (player1position > 100) {
                 player1position -= number;
             }
+            System.out.println("Player position after " + diceRollCounter + " is " + player1position);
         }
 
         System.out.println("Final position " + player1position);
