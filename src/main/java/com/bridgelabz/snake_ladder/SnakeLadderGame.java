a921d53b26d5045034ada920fd4bee2f071efdbf
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 15:31:13 +0530

    Added: Code to reach winning position 100

--- src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
+++ src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
@@ -2,20 +2,24 @@ package com.bridgelabz.snake_ladder;
 
 public class SnakeLadderGame {
     public static void main(String[] args) {
-
         int player1position = 0;
-        int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
-        int option = (int) Math.floor(Math.random() * 10) % 3;
-        switch (option) {
-            case 0:
-                player1position = player1position;
-                break;
-            case 1:
-                player1position += number;
-                break;
-            case 2:
-                player1position -= number;
-                break;
+        while (player1position < 100) {
+            int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
+            int option = (int) Math.floor(Math.random() * 10) % 3;
+            switch (option) {
+                case 0:
+                    player1position = player1position;
+                    break;
+                case 1:
+                    player1position += number;
+                    break;
+                case 2:
+                    player1position -= number;
+                    break;
+            }
+            if (player1position < 0) {
+                player1position = 0;
+            }
         }
         System.out.println(player1position);
     }
