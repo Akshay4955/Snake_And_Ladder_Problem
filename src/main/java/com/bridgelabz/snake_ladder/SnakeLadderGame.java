67de15bfd350e7ee4a84cb6bd1ec879ec45887df
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 21:34:00 +0530

    Added: Code to play with two players

--- src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
+++ src/main/java/com/bridgelabz/snake_ladder/SnakeLadderGame.java
@@ -1,34 +1,51 @@
 package com.bridgelabz.snake_ladder;
 
 public class SnakeLadderGame {
+    public static final int WINNING_POSITION = 100;
+    int diceCounter = 0;
+
     public static void main(String[] args) {
-        int player1position = 0, diceRollCounter = 0;
-        while (player1position < 100) {
-            int number = (int) Math.floor(Math.random() * 100) % 6 + 1;
-            diceRollCounter++;
-            int option = (int) Math.floor(Math.random() * 10) % 3;
-            switch (option) {
-                case 0:
-                    player1position = player1position;
-                    break;
-                case 1:
-                    player1position += number;
-                    break;
-                case 2:
-                    player1position -= number;
-                    break;
-            }
-
-            if (player1position < 0) {
-                player1position = 0;
-            }
-
-            if (player1position > 100) {
-                player1position -= number;
-            }
-            System.out.println("Player position after " + diceRollCounter + " is " + player1position);
+        System.out.println("Welcome to Snake and Ladder game");
+        int player1Position = 0, player2Position = 0;
+        SnakeLadderGame snakeLadderGame = new SnakeLadderGame();
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
+            player1Position = snakeLadderGame.playGame(player1Position);
+            //System.out.println("Player 1 position is " + player1Position);
+            player2Position = snakeLadderGame.playGame(player2Position);
+            //System.out.println("Player 2 position is " + player2Position);
         }
 
-        System.out.println("Final position " + player1position);
+        System.out.println("No of times dice rolled: " + snakeLadderGame.diceCounter);
+        if (player1Position > player2Position)
+            System.out.println("Winner is Player 1");
+        else
+            System.out.println("Winner is Player 2");
+    }
+
+    int playGame(int position) {
+        int diceNumber = (int) Math.floor(Math.random() * 100) % 6 + 1;
+        diceCounter++;
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                position += diceNumber;
+                position = playGame(position);
+                break;
+            case 2:
+                position -= diceNumber;
+                break;
+        }
+
+        if (position < 0) {
+            position = 0;
+        }
+
+        if (position > WINNING_POSITION) {
+            position -= diceNumber;
+        }
+
+        return position;
     }
 }
